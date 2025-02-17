964c18d75052410ae9a2aa40bb476bbcc39089f6
jrjuang <jrjuang00@gmail.com>
2025-02-17 19:36:15 +0800

    Test view space shader

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,35 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Environment, useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
 
+// #region view space shader
+import { extend } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+const SkyboxMaterial = shaderMaterial(
+  { time: 0 },
+  `
+    varying vec3 vWorldPosition;
+    void main() {
+      vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+      vWorldPosition = worldPosition.xyz;
+      gl_Position = projectionMatrix * viewMatrix * vec4(position, 1.0);
+    }
+  `,
+  `
+    precision mediump float;
+    varying vec3 vWorldPosition;
+
+    void main() {
+      vec3 color = vec3(0.1, 0.3, 0.6);
+      float gradient = smoothstep(-1.0, 1.0, normalize(vWorldPosition).y);
+      gl_FragColor = vec4(mix(vec3(0.8, 0.9, 1.0), color, gradient), 1.0);
+    }
+  `
+);
+
+extend({ SkyboxMaterial });
+// #endregion
+
+
 const Model = () => {
   const { scene } = useGLTF('Suzanne.glb')
   return <primitive object={scene} />
