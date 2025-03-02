653d3b859cced069a7d1e83de6b194ad24063fe9
jrjuang <jrjuang00@gmail.com>
2025-03-03 01:36:06 +0800

    Displaying the hover mask

--- src/Skybox.tsx
+++ src/Skybox.tsx
@@ -35,23 +35,12 @@ const SkyboxMaterial = shaderMaterial(
     uniform vec3 pointer;
 
     void main() {
-      //debug sky blue
-      // vec3 color = vec3(0.1, 0.3, 0.6);
-      // float gradient = smoothstep(-1.0, 1.0, pointer.y);
-      //gl_FragColor = vec4(mix(vec3(0.8, 0.9, 1.0), color, gradient), 1.0);
-      
       vec3 worldDirection = normalize(linearWorldPos);
       vec2 uv = sphereUV(pointer);
       vec2 uv_masks = sphereUV(worldDirection);
-      //debug testing
-      // gl_FragColor = vec4(uv, 0, sin(time * 0.5) * 0.5 + 0.5);
-
       vec4 selection = texture2D(masks, uv);
       vec4 masks = texture2D(masks, uv_masks);
-      //debug
-      // gl_FragColor = vec4(masks.rgb, sin(time * 0.5) * 0.5 + 0.5);
-      
-      gl_FragColor = selection.rgb == masks.rgb ? selection : vec4(0.0);
+      gl_FragColor = selection.rgb == masks.rgb ? vec4(selection.rgb, sin(time * 4.0) * 0.5 + 0.5) : vec4(0.0);
     }
   `
 );
@@ -68,9 +57,6 @@ const Skybox = ({ pointer }) => {
 
   const { camera } = useThree();
   useFrame(({ clock }) => {
-    //debug
-    console.log(`Pointer: ${pointer.direction.x}, ${pointer.direction.y}, ${pointer.direction.z}`);
-
     if (!ref.current) { return; }
     const material = ref.current.material;
     material.uniforms.time.value = clock.getElapsedTime();
