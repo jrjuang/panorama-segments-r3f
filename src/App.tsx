62ef0de23317b121a9c5ce67aefe27cf9a53ed74
jrjuang <jrjuang00@gmail.com>
2025-03-09 17:32:07 +0800

    Refactoring mouse pointer

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,17 @@ import * as THREE from 'three'
 import PanoScene from "./PanoScene.tsx"
 import FovControls from "./FovControls"
 
-const Model = () => {
-  const { scene } = useGLTF('Suzanne.glb')
-  return <primitive object={scene} />
-}
+// const Model = () => {
+//   const { scene } = useGLTF('Suzanne.glb')
+//   return <primitive object={scene} />
+// }
 
 const App = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const cameraRef = useRef<THREE.PerpectiveCamera | null>(null);
-  // Get the mouse cursor position
-  const cursorRay = { origin: new THREE.Vector3(), direction: new THREE.Vector3(1, 0, 0) };
-  useEffect(() => {
-    function mouseMove(event) {
-      const camera = cameraRef.current;
-      const canvas = canvasRef.current;
-      let cursorDir = new THREE.Vector3(event.clientX / canvas.width * 2 - 1, -event.clientY / canvas.height * 2 + 1, 0);
-      cursorDir.unproject(camera);
-      cursorDir = cursorDir.sub(camera.position);
-      cursorDir.normalize();
-      cursorRay.origin = camera.position;
-      cursorRay.direction = cursorDir;
-    }
-    const c = canvasRef.current;
-    c.addEventListener("mousemove", mouseMove);
-    return () => c.removeEventListener("mousemove", mouseMove);
-  }, []);
   return (
     <Canvas ref={canvasRef} style={{ height: '100vh', width: '100vw' }} camera={{ position: [0, 0, -1], fov: 90 }} onCreated={({ camera }) => cameraRef.current = camera} >
-      <PanoScene pointer={cursorRay}/>
+      <PanoScene />
       <ambientLight />
       <pointLight position={[1, 5, 2]} />
       <OrbitControls rotateSpeed={-0.2}/>
--- src/PanoScene.tsx
+++ src/PanoScene.tsx
@@ -64,27 +64,24 @@ const SkyboxMaterial = shaderMaterial(
 );
 extend({ SkyboxMaterial });
 
-const PanoScene = ({ pointer }: { pointer: { origin: THREE.Vector3, direction: THREE.Vector3 } }) => {
-  const [masks, setMasks] = useState<CanvasRenderingContext2D | null>(null);
-
+const PanoScene = () => {
   const masksPathOfExr: Map<string, string> = new Map();
   masksPathOfExr.set("studio.exr", "masks2_studio.png");
   masksPathOfExr.set("brown.exr", "masks2_brown.png");
   masksPathOfExr.set("bathroom.exr", "masks2_bathroom.png");
 
+  const masksLookup: HTMLCanvasElement = document.createElement("canvas");
   const textureLoader = new THREE.TextureLoader();
   function changeMasks(boxRef: React.RefObject<THREE.Mesh>, masksPath: string) {
     textureLoader.load(masksPath, (masks: THREE.Texture) => {
       if (boxRef.current) {
         boxRef.current.material.uniforms.masks.value = masks;
       }
-      const tempCanvas = document.createElement("canvas");
       const img_masks = masks.image;
-      tempCanvas.width = img_masks.width;
-      tempCanvas.height = img_masks.height;
-      const context = tempCanvas.getContext("2d");
+      masksLookup.width = img_masks.width;
+      masksLookup.height = img_masks.height;
+      const context = masksLookup.getContext("2d");
       context?.drawImage(img_masks, 0, 0);
-      setMasks(context);
     });
   }
 
@@ -98,13 +95,49 @@ const PanoScene = ({ pointer }: { pointer: { origin: THREE.Vector3, direction: T
 
   const boxRef = useRef<THREE.Mesh>(null);
   const [exrPath, setExrPath] = useState<string>("studio.exr");
+  const { camera, gl } = useThree();
+  useEffect(() => {
 
+  }, []);
+  // Switch the scene by clicking on a mask
+  useEffect(() => {
+    const mouseUpToSwitchScene = (event: MouseEvent) => {
+      const canvas = gl.domElement;
+      const masksContent = masksLookup.getContext("2d");
+      if (!masksContent) { return; }
+      const pointerDirection: THREE.Vector3
+        = new THREE.Vector3(
+          event.clientX / canvas.width * 2 - 1,
+          -event.clientY / canvas.height * 2 + 1,
+          0).unproject(camera).sub(camera.position);
+      pointerDirection.normalize();
+      const [u, v]: [number, number] = sphereUV(pointerDirection);
+      const x: number = Math.floor(u * masksLookup.width);
+      const y: number = Math.floor((1 - v) * masksLookup.height);
+      const pixel: Uint8ClampedArray = masksContent.getImageData(x, y, 1, 1).data;
+      if (0 === pixel[3]) { return; }
+      const mask: string = `${pixel[0]} ${pixel[1]} ${pixel[2]}`;
+      const selection: string = "not implemented yet";
+      console.log(`Mouse up at canvas ${event.clientX}, ${event.clientY};\n` +
+        `UV: ${u}, ${v}; position at masks: ${x}, ${y};\n` +
+        `mask: ${mask}; selection: ${selection}`);
+      setExrPath((prev: string) => {
+        console.log(`Not implemented yet to switch scene. Now the current scene is ${prev}`);
+        return prev;
+      });
+    };
+    gl.domElement.addEventListener("mouseup", mouseUpToSwitchScene);
+    return () => {
+      gl.domElement.removeEventListener("mouseup", mouseUpToSwitchScene);
+    }
+  }, []);
+  // Load the masks
   useEffect(() => {
     changeMasks(boxRef, String(masksPathOfExr.get(exrPath)));
-  }, [boxRef.current, exrPath]);
-
+  }, [exrPath]);
+  // Switch the scene by pressing F2
   useEffect(() => {
-    const handler = (event: KeyboardEvent) => {
+    const keyDownToSwitchScene = (event: KeyboardEvent) => {
       if (event.key === "F2") {
         setExrPath((prev: string) => {
           if (prev === "studio.exr") {
@@ -117,35 +150,18 @@ const PanoScene = ({ pointer }: { pointer: { origin: THREE.Vector3, direction: T
         });
       }
     };
-    window.addEventListener("keydown", handler);
-    return () => window.removeEventListener("keydown", handler);
+    window.addEventListener("keydown", keyDownToSwitchScene);
+    return () => window.removeEventListener("keydown", keyDownToSwitchScene);
   }, []);
-
-  const { camera } = useThree();
+  // Update the shader uniforms
   useFrame(({ clock }) => {
     if (!boxRef.current) { return; }
     const material = boxRef.current.material;
     material.uniforms.time.value = clock.getElapsedTime();
 
-    if (masks) {
-      const [u, v]: [number, number] = sphereUV(pointer.direction);
-      const width: number = masks.canvas.width;
-      const height: number = masks.canvas.height;
-      const x: number = Math.floor(u * width);
-      const y: number = Math.floor((1 - v) * height);
-      const pixel: Uint8ClampedArray = masks.getImageData(x, y, 1, 1).data;
-      // Used as an ID lable
-      console.log(`uv: ${u}, ${v}; pixel position: ${x}, ${y}; selection mask: ${pixel}`);
-
-      const r: number = pixel[0];
-      const g: number = pixel[1];
-      const b: number = pixel[2];
-      const a: number = pixel[3];
-      material.uniforms.selectionMask.value.set(r, g, b, a); // Not work as FX mask in shader due to Three.js does not support [0, 255] sampler2D
-    }
-    // For hover FX
-    material.uniforms.pointer.value.set(pointer.direction.x, pointer.direction.y, pointer.direction.z);
-
+    // Hover FX for a specific mask
+    // refcotring: material.uniforms.pointer.value.set(pointer.direction.x, pointer.direction.y, pointer.direction.z);
+    // Make the FX box follow the camera
     const pos = camera.position;
     boxRef.current.position.set(pos.x, pos.y, pos.z);
     const radius: number = 0.5 * (camera.near + camera.far)
